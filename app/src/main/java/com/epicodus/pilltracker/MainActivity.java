c8c4d4ccf6f0238502f27bbb39c3c289e96eac77
Abigail Rolling <arolling@gmail.com>
2016-04-22 10:35:43 -0700

    Add basic navigation drawer with success toast

--- app/src/main/java/com/epicodus/pilltracker/MainActivity.java
+++ app/src/main/java/com/epicodus/pilltracker/MainActivity.java
@@ -1,13 +1,44 @@
 package com.epicodus.pilltracker;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mNavChoices;
+    @Bind(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+    @Bind(R.id.navigation_drawer) ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mNavChoices = getResources().getStringArray(R.array.nav_choices);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mNavChoices));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            String page = ((TextView)view).getText().toString();
+            Toast.makeText(MainActivity.this, page, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
