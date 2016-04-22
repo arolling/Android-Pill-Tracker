49bad35be0ab8cdd11fc207ed290e2dd1ce2562f
Abigail Rolling <arolling@gmail.com>
2016-04-22 13:36:13 -0700

    Make add new medications button to move to that activity

--- app/src/main/java/com/epicodus/pilltracker/NewUserActivity.java
+++ app/src/main/java/com/epicodus/pilltracker/NewUserActivity.java
@@ -1,13 +1,37 @@
 package com.epicodus.pilltracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewUserActivity extends MainActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewUserActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.addMedicationsButton) Button mAddMedsButton;
+    @Bind(R.id.etUserName) EditText mUserName;
+    @Bind(R.id.etDoctorName) EditText mDoctorName;
+    @Bind(R.id.etDoctorAddress) EditText mDoctorAddress;
+    @Bind(R.id.etPharmacyName) EditText mPharmacyName;
+    @Bind(R.id.etPharmacyAddress) EditText mPharmacyAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+
+        mAddMedsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAddMedsButton) {
+            Intent intent = new Intent(NewUserActivity.this, NewMedicationActivity.class);
+            startActivity(intent);
+        }
     }
 }
